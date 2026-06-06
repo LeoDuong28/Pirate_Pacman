528991abf33f47e64b720bae95cba01efd60dae1
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-06-06 00:12:09 -0700

    Create cmake_clean.cmake

---
+++ build/CMakeFiles/PiratePacman.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PiratePacman.dir/src/AStar.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/AStar.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/AudioGen.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/AudioGen.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/FruitBonus.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/FruitBonus.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/Game.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/Game.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/Ghost.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/Ghost.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/InfluenceMap.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/InfluenceMap.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/Map.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/Map.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/Particle.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/Particle.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/Player.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/Player.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/ScorePopup.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/ScorePopup.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/SpriteSheet.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/SpriteSheet.cpp.o.d"
+  "CMakeFiles/PiratePacman.dir/src/main.cpp.o"
+  "CMakeFiles/PiratePacman.dir/src/main.cpp.o.d"
+  "PiratePacman"
+  "PiratePacman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PiratePacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
