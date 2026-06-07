f77e0614bb2dfa5722e91adb613e8092f9f3b57d
LeoDuong28 <duongtrongnghia287@gmail.com>
2026-06-07 01:18:18 -0700

    Create DependInfo.cmake

---
+++ build/CMakeFiles/PiratePacman.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/AStar.cpp" "CMakeFiles/PiratePacman.dir/src/AStar.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/AStar.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/AudioGen.cpp" "CMakeFiles/PiratePacman.dir/src/AudioGen.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/AudioGen.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/FruitBonus.cpp" "CMakeFiles/PiratePacman.dir/src/FruitBonus.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/FruitBonus.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/Game.cpp" "CMakeFiles/PiratePacman.dir/src/Game.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/Game.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/Ghost.cpp" "CMakeFiles/PiratePacman.dir/src/Ghost.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/Ghost.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/InfluenceMap.cpp" "CMakeFiles/PiratePacman.dir/src/InfluenceMap.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/InfluenceMap.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/Map.cpp" "CMakeFiles/PiratePacman.dir/src/Map.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/Map.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/Particle.cpp" "CMakeFiles/PiratePacman.dir/src/Particle.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/Particle.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/Player.cpp" "CMakeFiles/PiratePacman.dir/src/Player.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/Player.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/ScorePopup.cpp" "CMakeFiles/PiratePacman.dir/src/ScorePopup.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/ScorePopup.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/SpriteSheet.cpp" "CMakeFiles/PiratePacman.dir/src/SpriteSheet.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/SpriteSheet.cpp.o.d"
+  "/Users/nghiaduong/Documents/GitHub/Pirate_Pacman/src/main.cpp" "CMakeFiles/PiratePacman.dir/src/main.cpp.o" "gcc" "CMakeFiles/PiratePacman.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
